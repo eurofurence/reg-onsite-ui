8e0a78ed5b245d9c288b0fd972ce970e02405551
Rakan <rakanhowls@gmail.com>
2024-09-17 15:40:46 +0200

    Fixed that autoselection prevention kicks in only if one of the pair was autoselected

--- onsite/app/composables/logic/regdesk/handleAutoSelection.ts
+++ onsite/app/composables/logic/regdesk/handleAutoSelection.ts
@@ -3,6 +3,53 @@ import type {
   TransformedAttendeeInfo,
 } from "@/types/internal";
 
+const previousAutoSelectId: Ref<number | undefined> = ref(undefined);
+var previousTwinId: [number | null, number | null] | null = null;
+
+function performAutoSelection(
+  transformedAttendeeInfoList: TransformedAttendeeInfo[],
+  selectedAttendeeRef: Ref<TransformedAttendeeInfo | null>,
+  previousSelectId: Ref<number | undefined>
+) {
+  if (
+    transformedAttendeeInfoList.length > 2 ||
+    transformedAttendeeInfoList.length === 0
+  ) {
+    previousTwinId = null;
+    return;
+  }
+  if (transformedAttendeeInfoList.length === 2) {
+    previousTwinId = [
+      transformedAttendeeInfoList[0]?.id || null,
+      transformedAttendeeInfoList[1]?.id || null,
+    ];
+    return;
+  }
+  if (transformedAttendeeInfoList.length === 1) {
+    const single: TransformedAttendeeInfo = <TransformedAttendeeInfo>(
+      transformedAttendeeInfoList[0]
+    );
+    if (
+      previousAutoSelectId.value !== undefined &&
+      previousTwinId &&
+      previousTwinId.includes(previousAutoSelectId.value)
+    ) {
+      // Avoid accidental auto-selection after eg. a checkin has happened (github issue #3)
+      return;
+    }
+    if (
+      single.id !== null &&
+      single.id !== previousAutoSelectId.value &&
+      single.id !== previousSelectId.value
+    ) {
+      previousAutoSelectId.value = single.id;
+      selectedAttendeeRef.value = <TransformedAttendeeInfo>(
+        transformedAttendeeInfoList[0]
+      );
+    }
+  }
+}
+
 export function handleAutoSelection(
   transformedAttendeeListRef: Ref<TransformedAttendeeInfo[]>,
   selectedAttendeeRef: Ref<TransformedAttendeeInfo | null>,
@@ -10,41 +57,14 @@ export function handleAutoSelection(
   previousSelectId: Ref<number | undefined>
 ): void {
   // Handle autoselect
-  const previousAutoSelectId: Ref<number | undefined> = ref(undefined);
-  var previousTwinId: [number | null, number | null] | null = null;
   watch(
     () => transformedAttendeeListRef.value,
     async (
       value: TransformedAttendeeInfo[],
       _oldValue: TransformedAttendeeInfo[] | undefined
     ): Promise<void> => {
-      if (!displayOptionsRef.value.filterAutoSelect) {
-        return;
-      }
-      if (value.length > 2 || value.length === 0) {
-        previousTwinId = null;
-        return;
-      }
-      if (value.length === 2) {
-        previousTwinId = [value[0]?.id || null, value[1]?.id || null];
-        return;
-      }
-      if (value.length === 1) {
-        const single: TransformedAttendeeInfo = <TransformedAttendeeInfo>(
-          value[0]
-        );
-        if (previousTwinId && previousTwinId.includes(single.id)) {
-          // Avoid accidental auto-selection after eg. a checkin has happened (github issue #3)
-          return;
-        }
-        if (
-          single.id !== null &&
-          single.id !== previousAutoSelectId.value &&
-          single.id !== previousSelectId.value
-        ) {
-          previousAutoSelectId.value = single.id;
-          selectedAttendeeRef.value = <TransformedAttendeeInfo>value[0];
-        }
+      if (displayOptionsRef.value.filterAutoSelect) {
+        performAutoSelection(value, selectedAttendeeRef, previousSelectId);
       }
     },
     { immediate: true }
