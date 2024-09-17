f23455ea90b66f626621191ed2fbf07e87cf5d7d
Rakan <rakanhowls@gmail.com>
2024-09-17 16:44:34 +0200

    Use filter reset registry during full reset

--- onsite/app/composables/filter/doResetFilters.ts
+++ onsite/app/composables/filter/doResetFilters.ts
@@ -1,7 +1,9 @@
 import { defaultAttendeeDataOptions } from "@/config/regdesk";
 import type { AttendeeDataOptions } from "@/types/internal";
 import { deepCopy } from "@/composables/state/deepCopy";
+import { filterComponentRegistry } from "@/composables/state/filterComponentRegistry";
 
 export function doResetFilters(dataOptionsRef: Ref<AttendeeDataOptions>): void {
   dataOptionsRef.value.filters = deepCopy(defaultAttendeeDataOptions.filters);
+  filterComponentRegistry.callAllResets();
 }
