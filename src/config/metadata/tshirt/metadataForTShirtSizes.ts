1f8b01eb046a431f5233666178ddde64fad7e772
Rakan <rakanhowls@gmail.com>
2025-09-02 06:40:11 +0200

    Use the current reg value for XXL T-Shirt sizes and disable the old 2XL value

--- src/config/metadata/tshirt/metadataForTShirtSizes.ts
+++ src/config/metadata/tshirt/metadataForTShirtSizes.ts
@@ -7,7 +7,8 @@ export const enum TShirtSize {
   size_m = "M",
   size_l = "L",
   size_xl = "XL",
-  size_2xl = "2XL",
+  size_xxl = "XXL",
+  //size_2xl = "2XL",
   size_3xl = "3XL",
   size_4xl = "4XL",
 }
@@ -22,7 +23,8 @@ export const metadataRecordForTShirtSizes: MetadataRecord<
   [TShirtSize.size_m]: { label: "Medium" },
   [TShirtSize.size_l]: { label: "Large" },
   [TShirtSize.size_xl]: { label: "X-Large" },
-  [TShirtSize.size_2xl]: { label: "XX-Large" },
+  [TShirtSize.size_xxl]: { label: "XX-Large" },
+  //[TShirtSize.size_2xl]: { label: "XX-Large" },
   [TShirtSize.size_3xl]: { label: "3X-Large" },
   [TShirtSize.size_4xl]: { label: "4X-Large" },
 };
